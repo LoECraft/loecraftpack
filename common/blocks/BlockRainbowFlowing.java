614b8fc1ee6633e33aa4633b7b08f51a51b6518c
Nathan Powers <teknertechnologies@gmail.com>
2013-05-06 09:16:56 -0700

    Fixed Rundo's typos and removed unnecessary TODOs

--- common/blocks/BlockRainbowFlowing.java
+++ common/blocks/BlockRainbowFlowing.java
@@ -6,12 +6,11 @@ import net.minecraft.block.material.Material;
 public class BlockRainbowFlowing extends BlockFlowing
 {
 	//TODO  make various Types of colored water
-	//TODO  override an necessary code to make it compatible with other liquids
+	//TODO  override a necessary code to make it compatible with other liquids
 	//TODO  use/control ID in addition to MetaData, when calculating flow
 	public BlockRainbowFlowing(int par1, Material par2Material)
 	{
 		super(par1, par2Material);
-		// TODO Auto-generated constructor stub
 	}
 
 }
--- common/blocks/BlockRainbowFluid.java
+++ common/blocks/BlockRainbowFluid.java
@@ -6,11 +6,10 @@ import net.minecraft.block.material.Material;
 public class BlockRainbowFluid extends BlockFluid
 {
 	//TODO  make various Types of colored water
-	//TODO  override an necessary code to make it compatible with other liquids
+	//TODO  override a necessary code to make it compatible with other liquids
 	public BlockRainbowFluid(int par1, Material par2Material)
 	{
 		super(par1, par2Material);
-		// TODO Auto-generated constructor stub
 	}
 
 }
