aa81474ad4cfd3d7a648ddfecbfa9812abec6dd1
Nathan Powers <teknertechnologies@gmail.com>
2013-05-08 11:02:05 -0700

    Minor change to ItemBits

--- common/items/ItemBits.java
+++ common/items/ItemBits.java
@@ -15,7 +15,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 public class ItemBits extends Item
 {
 	public static final String[] names = {"1 Bit", "5 Bits", "10 Bits", "25 Bits", "100 Bits"};
-	public static final String[] iconNames = {"1bit", "5bits", "10bits", "25bits", "100bits"};
+	public static final String[] iconNames = new String[names.length];
 	public static final int num = names.length-1;
 	
 	@SideOnly(Side.CLIENT)
@@ -28,6 +28,9 @@ public class ItemBits extends Item
 		this.setMaxDamage(0);
         this.setCreativeTab(LoECraftPack.LoECraftTab);
         this.setUnlocalizedName("itemBits");
+        
+        for(int i = 0; i < iconNames.length; i++)
+        	iconNames[i] = names[i].replace(" ", "").toLowerCase();
 	}
 
 	@Override
