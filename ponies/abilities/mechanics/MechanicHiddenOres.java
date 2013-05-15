54ff19ae346181d816f5f2425e00efd32533b0e7
Tyler Purcell <nalov0@yahoo.com>
2013-05-15 16:22:06 -0700

    derp
    I really show look at my code more before committing....

--- ponies/abilities/mechanics/MechanicHiddenOres.java
+++ ponies/abilities/mechanics/MechanicHiddenOres.java
@@ -19,12 +19,15 @@ public class MechanicHiddenOres {
 		{
 			revealHiddenGems = !revealHiddenGems;
 			WorldRenderer[] worldRenderer = (WorldRenderer[])PrivateAccessor.getPrivateObject(Minecraft.getMinecraft().renderGlobal, "worldRenderers");
+			List worldRenderersToUpdate = (List)PrivateAccessor.getPrivateObject(Minecraft.getMinecraft().renderGlobal, "worldRenderersToUpdate");
 			for(int i=0; i < worldRenderer.length; i++)
 			{
-				((List)PrivateAccessor.getPrivateObject(Minecraft.getMinecraft().renderGlobal, "worldRenderersToUpdate")).add(worldRenderer[i]);
-				if(worldRenderer[i] != null)worldRenderer[i].markDirty();
+				if(worldRenderer[i] != null)
+				{
+					worldRenderersToUpdate.add(worldRenderer[i]);
+					worldRenderer[i].markDirty();
+				}
 			}
-			//
 			System.out.print("mode switch - "+ revealHiddenGems+" - got list - "+(worldRenderer != null));
 		}
 	}
