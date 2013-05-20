4e013f3bc2633465ef2dd0601855cfad47988158
Tyler Purcell <nalov0@yahoo.com>
2013-05-20 02:28:44 -0700

    will it work now?

---
+++ packet/NetworkedPotions.java
@@ -0,0 +1,21 @@
+package loecraftpack.packet;
+
+import java.util.HashMap;
+
+import loecraftpack.LoECraftPack;
+import cpw.mods.fml.common.network.PacketDispatcher;
+
+public class NetworkedPotions
+{
+	
+	public static final byte oreVision = -128;
+	
+	public static HashMap potions = new HashMap<Byte, Integer>(){{
+		put(oreVision, LoECraftPack.potionOreVision.id);
+	}};
+	
+	public static void applyEffect(byte potionId, int duration, int strength)
+	{
+		PacketDispatcher.sendPacketToServer(PacketHelper.Make("loecraftpack", PacketIds.applyPotionEffect, potionId, duration, (byte)strength));
+	}
+}
