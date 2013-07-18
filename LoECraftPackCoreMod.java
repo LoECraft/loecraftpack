dcb1debbfab5b99219a281afc18e3932ab525570
Rundo <nalov0@yahoo.com>
2013-07-17 20:09:29 -0700

    almost got ASM to behave
    the problem is I'm not sure if it actually does anything with the changes is prepares to make.
    thus the ASM still doesn't do what I need it to.

--- LoECraftPackCoreMod.java
+++ LoECraftPackCoreMod.java
@@ -21,7 +21,6 @@ import cpw.mods.fml.common.versioning.VersionRange;
 
 public class LoECraftPackCoreMod implements ModContainer {
 	
-	/*******************************************************************************************/
 	
 	public static DataWatcher getDataWatcher(Entity instance)
 	{
