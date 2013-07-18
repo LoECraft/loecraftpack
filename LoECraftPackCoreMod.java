32e873723e816007e905dcf231f91636f9d7d381
Rundo <nalov0@yahoo.com>
2013-07-17 22:42:14 -0700

    did more prep-work towards converting to ASM
    filled out config
    made use of existing DummyModContainer

--- LoECraftPackCoreMod.java
+++ LoECraftPackCoreMod.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Field;
 
 import java.io.File;
 import java.security.cert.Certificate;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
@@ -11,136 +12,48 @@ import net.minecraft.entity.DataWatcher;
 import net.minecraft.entity.Entity;
 
 import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
 
+import cpw.mods.fml.common.DummyModContainer;
 import cpw.mods.fml.common.LoadController;
 import cpw.mods.fml.common.MetadataCollection;
 import cpw.mods.fml.common.ModContainer;
 import cpw.mods.fml.common.ModMetadata;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.versioning.ArtifactVersion;
 import cpw.mods.fml.common.versioning.VersionRange;
 
-public class LoECraftPackCoreMod implements ModContainer {
+public class LoECraftPackCoreMod extends DummyModContainer {
+
+	public LoECraftPackCoreMod()
+	{
+		super(new ModMetadata());
+		/* ModMetadata is the same as mcmod.info */
+		ModMetadata myMeta = super.getMetadata();
+		myMeta.authorList = Arrays.asList(new String[] { "Rundo0" });
+		myMeta.description = "LoECraftPack's access transformer mod";
+		myMeta.modId = "loecraftpackcore";
+		myMeta.version = "1.0";
+		myMeta.name = "LoECraft Pack Core";
+        myMeta.url = "";
+	}
+	
+	public boolean registerBus(EventBus bus, LoadController controller)
+	{
+		bus.register(this);
+		return true;
+	}
+	
+	/*
+	 * Use this in place of @Init, @Preinit, @Postinit in the file.
+	 */
+	@Subscribe                 /* Remember to use the right event! */
+	public void onServerStarting(FMLServerStartingEvent ev)
+	{
+		ev.getServer().worldServerForDimension(0).spawnHostileMobs = false;
+		       
+	}
 	
 	
-	/*******************************************************************************************/
-	/*******************************************************************************************/
-	/*******************************************************************************************/
-	
-	@Override
-	public String getModId() {
-		return "loecraftpackcore";
-	}
-
-	@Override
-	public String getName() {
-		return "LoECraft Pack Core";
-	}
-
-	@Override
-	public String getVersion() {
-		return "1.0";
-	}
-
-	@Override
-	public File getSource() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ModMetadata getMetadata() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void bindMetadata(MetadataCollection mc) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setEnabledState(boolean enabled) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Set<ArtifactVersion> getRequirements() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<ArtifactVersion> getDependencies() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<ArtifactVersion> getDependants() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getSortingRules() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean registerBus(EventBus bus, LoadController controller) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean matches(Object mod) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public Object getMod() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ArtifactVersion getProcessedVersion() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean isImmutable() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isNetworkMod() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public String getDisplayVersion() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public VersionRange acceptableMinecraftVersionRange() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Certificate getSigningCertificate() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
