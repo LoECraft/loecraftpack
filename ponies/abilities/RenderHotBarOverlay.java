784174cf142a6dd64cee916be7168e1ad0f0dc68
Rundo <nalov0@yahoo.com>
2013-07-20 22:14:41 -0700

    derp
    does not need to affect cooldown

--- ponies/abilities/RenderHotBarOverlay.java
+++ ponies/abilities/RenderHotBarOverlay.java
@@ -45,17 +45,18 @@ public class RenderHotBarOverlay
         	GL11.glEnable(GL11.GL_BLEND);
             GL11.glBlendFunc(GL11.GL_SRC_COLOR, GL11.GL_ONE_MINUS_SRC_COLOR);
         }
-        
         GL11.glColor4f(255.0F, 255.0F, 255.0F, 255.0f);
         
         if(!this.mc.thePlayer.capabilities.isCreativeMode)
         	renderEnergyBar(width, height);
         
         renderChargeBar(width, height);
-        renderCoolDowns(width, height);
         
         if (this.mc.currentScreen instanceof GuiChat)
         	GL11.glDisable(GL11.GL_BLEND);
+        
+        renderCoolDowns(width, height);
+        
         RenderHelper.disableStandardItemLighting();
         GL11.glDisable(GL12.GL_RESCALE_NORMAL);
         this.mc.mcProfiler.endSection();
